175cea3637f11f7ebe6b6560583b5643d29024f8
jzhang03 <jzhang03@DESKTOP-KASGRPT>
2020-10-28 13:01:44 -0400

    HW3: Re-do the HW1-2 with ANT

---
+++ HW3/test/src/edu/umb/cs680/hw02/PrimeGeneratorTest.java
@@ -0,0 +1,44 @@
+package edu.umb.cs680.hw02;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+
+public class PrimeGeneratorTest {
+	
+	@Test
+    public void primeFrom1To10() {
+        PrimeGenerator gen = new PrimeGenerator(1, 10);
+        gen.generatePrimes();
+        Long[] expectedPrimes = { 2L, 3L, 5L, 7L };
+        assertArrayEquals(expectedPrimes, gen.getPrimes().toArray());
+    }
+
+    @Test
+    public void primeFrom50To75() {
+        PrimeGenerator gen = new PrimeGenerator(50, 75);
+        gen.generatePrimes();
+        Long[] expectedPrimes = { 53L, 59L, 61L, 67L, 71L, 73L};
+        assertArrayEquals(expectedPrimes, gen.getPrimes().toArray());
+    }
+
+    @Test
+    public void primeFromNegative10To10() {
+        try{
+            PrimeGenerator gen = new PrimeGenerator(-10, 10);
+            fail("Out of range");
+        } catch(IllegalArgumentException ex) {
+            assertEquals("Start number should bigger than 0", ex.getMessage());
+        }
+    }
+
+    @Test
+    public void primeFrom100To1() {
+        try {
+            PrimeGenerator gen = new PrimeGenerator(100, 1);
+            fail("Out of range");
+        } catch (IllegalArgumentException ex) {
+            assertEquals("Start number shoule be smaller than end number", ex.getMessage());
+        }
+    }
+}
