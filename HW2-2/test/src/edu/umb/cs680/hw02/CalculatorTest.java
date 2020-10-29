98fbdf74a37e1bc79d4b98c554aceb0e144b2d83
jzhang03 <jzhang03@DESKTOP-KASGRPT>
2020-10-29 10:02:56 -0400

    HW2-2: Copyright update

--- HW2-2/test/src/edu/umb/cs680/hw02/CalculatorTest.java
+++ HW2-2/test/src/edu/umb/cs680/hw02/CalculatorTest.java
@@ -1,3 +1,10 @@
+//
+// CS680: HW2-2
+// Copyright 2020 Jing Zhang <Jing.Zhang002@umb.edu>
+// Git Repositories: https://github.com/jzhang03/CS680_JingZhang
+// Git Name: jzhang03
+//
+
 package edu.umb.cs680.hw02;
 
 import static org.junit.jupiter.api.Assertions.*;
