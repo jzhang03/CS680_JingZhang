eea1fbb7edd6922a61ff4b348e2c8adbe5705b34
jzhang03 <jzhang03@DESKTOP-KASGRPT>
2020-10-29 09:53:09 -0400

    HW5: Define the Car class and implement its getter methods with JUNIT.

---
+++ HW5/src/edu/umb/cs680/hw05/Car.java
@@ -0,0 +1,42 @@
+//
+// CS680: HW5
+// Copyright 2020 Jing Zhang <Jing.Zhang002@umb.edu>
+// Git Repositories: https://github.com/jzhang03/CS680_JingZhang
+// Git Name: jzhang03
+//
+
+package edu.umb.cs680.hw05;
+
+public class Car {
+    private String make, model;
+    private int mileage, year;
+    private float price;
+
+    public Car(String make, String model, int mileage, int year, float price) {
+        this.make = make;
+        this.model = model;
+        this.mileage = mileage;
+        this.year = year;
+        this.price = price;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+}
---
+++ HW5/test/src/edu/umb/cs680/hw05/CarTest.java
@@ -0,0 +1,41 @@
+//
+// CS680: HW5
+// Copyright 2020 Jing Zhang <Jing.Zhang002@umb.edu>
+// Git Repositories: https://github.com/jzhang03/CS680_JingZhang
+// Git Name: jzhang03
+//
+
+package edu.umb.cs680.hw05;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import edu.umb.cs680.hw05.Car;
+
+
+public class CarTest {
+    private String[] carToStringArray(Car car) {
+        String[] carInfo = {car.getMake(),car.getModel(),Integer.toString(car.getYear())};
+        return carInfo;
+    }
+
+    @Test
+    public void verifyCarEqualityWithMakeModelYear1() {
+         String[] expected = {"Toyota", "RAV4", "2018"}; 
+         Car actual = new Car("Toyota", "RAV4", 1000, 2018, 22000f); 
+         assertArrayEquals(expected, carToStringArray(actual));
+    }
+    
+    @Test
+    public void verifyCarEqualityWithMakeModelYear2() {
+        String[] expected = {"Lexus", "RX450h", "2015"};
+        Car actual = new Car("Lexus", "RX450h", 25000, 2015, 37000f);
+        assertArrayEquals(expected, carToStringArray(actual));
+    }
+    
+    @Test
+    public void verifyCarEqualityWithMakeModelYear3() {
+        String[] expected = {"Toyota", "RAV4", "2017"};
+        Car actual = new Car("Acura", "RDX", 6000, 2017, 34000f);
+        assertArrayEquals(expected, carToStringArray(actual));
+    }
+}
